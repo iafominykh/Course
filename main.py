1ab538ec6275adc7a147faea90580febb349e15e
iafominykh <iafominykh@yandex.ru>
2023-04-19 19:49:58 +0300

    add tests

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from utils.utils import load_json, executed_operation, formated_data
 
-JSON_FILE = '../operations.json'
+JSON_FILE = '/home/test/PycharmProjects/Course//operations.json'
+
 
 def main():
-
     data = load_json(JSON_FILE)
     data = executed_operation(data)
 
     for i in range(5):
         print(formated_data(data[i]))
-
-
 
 
 if __name__ == '__main__':
