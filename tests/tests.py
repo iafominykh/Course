5757034d6c062cad5b8031c79f8fb4dfe8eda6fd
iafominykh <iafominykh@yandex.ru>
2023-04-19 17:51:45 +0300

    add tests

--- tests/tests.py
+++ tests/tests.py
@@ -21,6 +21,41 @@ def test_load_json():
     assert load_json('test.json') == list_
 
 
+def test_executed_operation():
+    list_ = [
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2019-08-19T04:27:37.904916"
+        },
+        {
+            "id": 476991061,
+            "state": "CANCELED",
+            "date": "2018-11-23T17:47:33.127140"
+        },
+        {
+            "id": 649467725,
+            "state": "EXECUTED",
+            "date": "2021-04-14T19:35:28.978265"
+        }
+    ]
+
+    sorted_list = [
+        {
+            "id": 649467725,
+            "state": "EXECUTED",
+            "date": "2021-04-14T19:35:28.978265"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2019-08-19T04:27:37.904916"
+        }
+
+    ]
+
+    assert executed_operation(list) == sorted_list
+
 
 def test_format_date():
     assert format_date('2018-10-14T08:21:33.419441') == '14.10.2018'
